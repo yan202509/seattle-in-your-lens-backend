66a604073d696226baaacd4fcaf93777683eeaf8
YanYi Chen <vickyeah13@gmail.com>
2026-01-23 01:41:24 -0800

    set up and created database

---
+++ src/main/java/com/adacapstone/seattleinyourlens/SeattleInYourLensApplication.java
@@ -0,0 +1,13 @@
+package com.adacapstone.seattleinyourlens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeattleInYourLensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeattleInYourLensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adacapstone/seattleinyourlens/SeattleInYourLensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adacapstone.seattleinyourlens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeattleInYourLensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
