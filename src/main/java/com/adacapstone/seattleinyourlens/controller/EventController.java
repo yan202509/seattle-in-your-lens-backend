2d0676f838d1cccbbcfe240586c7ba363594cc00
YanYi Chen <vickyeah13@gmail.com>
2026-01-27 01:30:48 -0800

    EventService and EventController, now can post and get events

--- src/main/java/com/adacapstone/seattleinyourlens/controller/EventController.java
+++ src/main/java/com/adacapstone/seattleinyourlens/controller/EventController.java
@@ -1,4 +1,30 @@
 package com.adacapstone.seattleinyourlens.controller;
 
+
+import com.adacapstone.seattleinyourlens.entity.Event;
+import com.adacapstone.seattleinyourlens.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+@RequiredArgsConstructor
 public class EventController {
+
+    private final EventService eventService;
+
+
+    // Create a new event
+    @PostMapping
+    public Event createEvent(@RequestBody Event event) {
+        return eventService.postEvent(event);
+    }
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+
 }
--- src/main/java/com/adacapstone/seattleinyourlens/service/EventService.java
+++ src/main/java/com/adacapstone/seattleinyourlens/service/EventService.java
@@ -1,10 +1,24 @@
 package com.adacapstone.seattleinyourlens.service;
 
 
+import com.adacapstone.seattleinyourlens.entity.Event;
+import com.adacapstone.seattleinyourlens.repository.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public Event postEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
 }
