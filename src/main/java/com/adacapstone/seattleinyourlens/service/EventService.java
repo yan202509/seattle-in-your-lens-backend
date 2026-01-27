2a9b6b82dc31b56f969362f1288ae5c8374eef37
YanYi Chen <vickyeah13@gmail.com>
2026-01-27 00:30:51 -0800

    clean up application properties

--- src/main/java/com/adacapstone/seattleinyourlens/service/EventService.java
+++ src/main/java/com/adacapstone/seattleinyourlens/service/EventService.java
@@ -1,8 +1,10 @@
 package com.adacapstone.seattleinyourlens.service;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EventService {
 }
